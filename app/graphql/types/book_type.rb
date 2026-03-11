2849036f44b25f8f2f234c5eaf8ba2486ed3e249
wisdom-plus <wisdom-plus@users.noreply.github.com>
2026-03-12 08:33:23 +0900

    add:comment backend

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -10,5 +10,7 @@ module Types
     field :description, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :comments, [Types::CommentType], null: false
   end
 end
