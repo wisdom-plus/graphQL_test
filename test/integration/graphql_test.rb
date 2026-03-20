ed6f27ddd18514c5a6cfc2021be026f21ea6031b
wisdom-plus <wisdom-plus@users.noreply.github.com>
2026-03-21 08:54:12 +0900

    add:graphql test

--- test/integration/graphql_test.rb
+++ test/integration/graphql_test.rb
@@ -7,4 +7,43 @@ class GraphqlTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal "Hello from Rails GraphQL", response.parsed_body.dig("data", "hello")
   end
+
+  test "creates a book from graphql mutation" do
+    assert_difference("Book.count", 1) do
+      post graphql_path,
+           params: {
+             query: <<~GRAPHQL,
+               mutation CreateBook($input: BookInputType!) {
+                 createBook(user: $input) {
+                   id
+                   title
+                   author
+                   isbn
+                   publishedDate
+                   description
+                 }
+               }
+             GRAPHQL
+             variables: {
+               input: {
+                 title: "Frontend Mutation Book",
+                 author: "Codex",
+                 isbn: "9781234567890",
+                 publishedDate: "2026-03-29",
+                 description: "Created through GraphQL mutation."
+               }
+             }
+           },
+           as: :json
+    end
+
+    assert_response :success
+    created_book = response.parsed_body.dig("data", "createBook")
+
+    assert_equal "Frontend Mutation Book", created_book["title"]
+    assert_equal "Codex", created_book["author"]
+    assert_equal "9781234567890", created_book["isbn"]
+    assert_equal "2026-03-29", created_book["publishedDate"]
+    assert_equal "Created through GraphQL mutation.", created_book["description"]
+  end
 end
