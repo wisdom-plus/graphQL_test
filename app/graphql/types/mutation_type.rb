17b5795da9877b7b8af20fb0cfe2b62d94bdfee6
wisdom-plus <wisdom-plus@users.noreply.github.com>
2026-03-30 09:33:31 +0900

    add:delete mutaiton to mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_book, mutation: Mutations::CreateBook
     field :create_comment, mutation: Mutations::CreateComment
+    field :delete_comment, mutation: Mutations::DeleteComment
   end
 end
