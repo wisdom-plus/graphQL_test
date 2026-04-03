047e13d03c6ca9796fb177f84da339b797ea6d54
wisdom-plus <wisdom-plus@users.noreply.github.com>
2026-04-03 09:23:21 +0900

    fix:delete_comment return value

--- app/graphql/mutations/delete_comment.rb
+++ app/graphql/mutations/delete_comment.rb
@@ -1,12 +1,16 @@
 class Mutations::DeleteComment < GraphQL::Schema::Mutation
-  type Types::CommentType
+  type Types::DeleteCommentPayload
 
   argument :comment_id, ID, required: true
 
   def resolve(comment_id:)
-    comment = Comment.find(comment_id)
-    return comment if comment.destroy
+    comment = Comment.find_by(id: comment_id)
+    { deleted_comment_id: nil, success: false, errors: ['comment not found'] } if comment.blank?
 
-    nil
+    if comment.destroy
+      { deleted_comment_id: comment_id, success: true, errors: [] }
+    else
+      { deleted_comment_id: nil, success: false, errors: comment.errors.full_messages }
+    end
   end
 end
